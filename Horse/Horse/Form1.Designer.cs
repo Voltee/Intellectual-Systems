46e0dc35971b77e851a0eb067a057944aadf9c9f
Voltee <AlexeyDale@gmail.com>
2018-04-01 00:39:37 +0500

    Horse
    http://is.ifmo.ru/download/knight.pdf

---
+++ Horse/Horse/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Horse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Panel_Field = new System.Windows.Forms.FlowLayoutPanel();
+            this.n_X = new System.Windows.Forms.NumericUpDown();
+            this.n_Y = new System.Windows.Forms.NumericUpDown();
+            this.btn_populateField = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.n_speed = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.n_X)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n_Y)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n_speed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Panel_Field
+            // 
+            this.Panel_Field.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Panel_Field.Location = new System.Drawing.Point(12, 12);
+            this.Panel_Field.Name = "Panel_Field";
+            this.Panel_Field.Size = new System.Drawing.Size(500, 500);
+            this.Panel_Field.TabIndex = 0;
+            // 
+            // n_X
+            // 
+            this.n_X.Location = new System.Drawing.Point(536, 12);
+            this.n_X.Name = "n_X";
+            this.n_X.Size = new System.Drawing.Size(44, 20);
+            this.n_X.TabIndex = 1;
+            // 
+            // n_Y
+            // 
+            this.n_Y.Location = new System.Drawing.Point(610, 12);
+            this.n_Y.Name = "n_Y";
+            this.n_Y.Size = new System.Drawing.Size(44, 20);
+            this.n_Y.TabIndex = 2;
+            // 
+            // btn_populateField
+            // 
+            this.btn_populateField.Location = new System.Drawing.Point(518, 38);
+            this.btn_populateField.Name = "btn_populateField";
+            this.btn_populateField.Size = new System.Drawing.Size(154, 23);
+            this.btn_populateField.TabIndex = 3;
+            this.btn_populateField.Text = "Создать поле";
+            this.btn_populateField.UseVisualStyleBackColor = true;
+            this.btn_populateField.Click += new System.EventHandler(this.btn_populateField_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(519, 68);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(163, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Выбор расположения коня";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(518, 117);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(154, 23);
+            this.btn_start.TabIndex = 5;
+            this.btn_start.Text = "Запуск";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // n_speed
+            // 
+            this.n_speed.Location = new System.Drawing.Point(519, 91);
+            this.n_speed.Name = "n_speed";
+            this.n_speed.Size = new System.Drawing.Size(61, 20);
+            this.n_speed.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(586, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Скорость в мс";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 522);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.n_speed);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btn_populateField);
+            this.Controls.Add(this.n_Y);
+            this.Controls.Add(this.n_X);
+            this.Controls.Add(this.Panel_Field);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.n_X)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n_Y)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n_speed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel Panel_Field;
+        private System.Windows.Forms.NumericUpDown n_X;
+        private System.Windows.Forms.NumericUpDown n_Y;
+        private System.Windows.Forms.Button btn_populateField;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.NumericUpDown n_speed;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Horse/Horse/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Horse
+{
+    public partial class Form1 : Form
+    {
+        public int xBound, yBound;
+        public int xCurrent, yCurrent;
+        List<Label> fields;
+        List<string> history;
+        string lastNode = "1";
+        Timer timer;
+        Random rnd;
+        int moveCount = 1;
+        int attempt = 0;
+        bool isClosedPath = false;
+        bool isRandPath = true;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            moveCount++;
+            
+            timer = new Timer();
+            timer.Interval = (int)n_speed.Value;
+            timer.Tick += OnTick;
+            timer.Start();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rnd = new Random();
+        }
+
+        private void btn_populateField_Click(object sender, EventArgs e)
+        {
+            fields = new List<Label>();
+            Panel_Field.Controls.Clear();
+            xBound =(int)n_X.Value;
+            yBound = (int)n_Y.Value;
+            Size size = new Size(Panel_Field.Size.Width / xBound -10 , Panel_Field.Size.Height / yBound - 10 );
+            for (int i = 1; i <= yBound; i++)
+            {
+                for (int j = 1; j <= xBound; j++)
+                {
+                    Label l = new Label();
+                    l.Name = string.Format("{0}_{1}",j,i);
+                    l.Size = size;
+                    l.Text = "";
+                    l.BorderStyle = BorderStyle.Fixed3D;
+                    l.BackColor = Color.LightGray;
+                    l.Click += onClick;
+                    fields.Add(l);
+                    Panel_Field.Controls.Add(l);
+                }
+
+            }
+
+
+        }
+
+
+        void onClick(object sender, EventArgs e) {
+            if (checkBox1.Checked)
+            {
+                foreach (Label field in fields)
+                {
+                    field.BackColor = Color.LightGray;
+                    field.Text = "";
+                }
+                Label l = (Label)sender;
+                l.BackColor = Color.Orange;
+                l.Text = moveCount.ToString();
+                string[] coord = l.Name.Split('_');
+                xCurrent = Convert.ToInt32(coord[0]);
+                yCurrent = Convert.ToInt32(coord[1]);
+                Console.WriteLine(xCurrent + "_" + yCurrent);
+            }
+        }
+
+        void OnTick(object sender, EventArgs e)
+        {
+            if (isRandPath)
+            MoveHorseRandom();
+        }
+
+        void MoveHorseRandom()
+        {
+
+            int x = 0, y = 0;
+            int xNext = xCurrent, yNext = yCurrent;
+            while (!checkForFieldHistory(xNext, yNext))
+            {
+                x = 0;
+                xNext = xCurrent;
+                yNext = yCurrent;
+                while(x == 0)
+                {
+                    x = rnd.Next(-2, 3);
+                } 
+                switch (x)
+                {
+                    case 1:
+                        y = getSign(2);
+                        break;
+                    case 2:
+                        y = getSign(1);
+                        break;
+                    case -1:
+                        y = getSign(2);
+                        break;
+                    case -2:
+                        y = getSign(1);
+                        break;
+                }
+                xNext += x;
+                yNext += y;
+                Console.WriteLine(string.Format("{0}_{1}", x, y));
+                attempt++;
+                if (attempt == 150)
+                {
+                    isRandPath = false;
+                    return;
+                }
+            }
+            //Console.WriteLine(string.Format("{0}_{1}", xNext, yNext));
+            attempt = 0;
+            foreach (Label field in fields)
+            {
+                if (field.Name == string.Format("{0}_{1}", xNext, yNext))
+                {
+                    field.BackColor = Color.Orange;
+                    field.Text = moveCount.ToString();
+                    moveCount++;
+                    xCurrent = xNext;
+                    yCurrent = yNext;
+                }
+                else
+                    field.BackColor = Color.LightGray;
+            }
+
+        }
+
+        bool checkForFieldHistory(int x, int y)
+        {
+            if (x < 1 || x >= xBound + 2) return false;
+            else if (y < 1 || y >= yBound + 2) return false;
+            Label l = fields.Find(p => p.Name == string.Format("{0}_{1}", x, y));
+            if (l == null || l.Text != "") return false;
+            else return true;
+        }
+
+        int getSign(int number)
+        {
+            int r = rnd.Next(0, 2);
+            switch (r)
+            {
+                case 0:
+                    return number;
+
+                case 1:
+                    return number = Convert.ToInt32("-"+number);
+            }
+            return number;
+        }
+
+        void reOrginizePath()
+        {
+            Label node = fields.Find(p => p.Text == string.Format("{0}", lastNode));
+            int xNode;
+            int yNode;
+            for (int j = history.Count - 1; j > 0; j--)
+                {
+                Label cmp = fields.Find(p => p.Text == string.Format("{0}", history[j]));
+                xCmp;
+                yCmp;
+                
+                }
+            
+        }
+    }
+}
